61f160e0c590ad40d72dc9525ea6c62026bb362e
Henry <44884792+hoomji@users.noreply.github.com>
2022-09-25 22:24:36 -0400

    init

--- constants/blockchain.ts
+++ constants/blockchain.ts
@@ -1,4 +1,9 @@
-export const nftCollection = [
+export const nftCollections = [
+  "https://opensea.io/collection/anifty",
+  "https://opensea.io/collection/digidaigaku",
+  "https://opensea.io/collection/boredapeyachtclub",
+  "https://opensea.io/collection/azukis",
+  'https://opensea.io/collection/kumaleon-nfts'
 ];
 
 export type NftCollection = {
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,25 @@
 import axios from "axios";
+import Image from "next/image";
 
 function HomePage() {
   return (
     <div>
       <h1>Higher Or Lower With Uniblock</h1>
+      <div style={{ display: "flex", flexDirection: "column" }}>
+        <h2>NFT 1</h2>
+        <h2>vs.</h2>
+        <h2>NFT 2</h2>
+        <image></image>
+      </div>
+      <div style={{ display: "flex", paddingBottom: 10 }}>
+        <button>Higher</button>
+        <div style={{ padding: 5 }}></div>
+        <button>Lower</button>
+      </div>
+      <div style={{ paddingBottom: 10 }}>
+        <text>Score: 0</text>
+      </div>
+      <button>Start Game</button>
     </div>
   );
 }
